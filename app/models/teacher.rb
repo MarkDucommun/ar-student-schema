fce4df36a16f656852bc4c8ae19f37def7a13104
dbc-apprentice <apprentice@devbootcamp.com>
2013-07-31 19:51:13 -0500

    added some teachers

--- app/models/teacher.rb
+++ app/models/teacher.rb
@@ -2,6 +2,7 @@ require_relative '../../db/config'
 
 class Teacher < ActiveRecord::Base
 
-  attr_accessor
-
+  attr_accessor :first_name, :last_name, :email, :phone
+  
+  def initialize(args)
 end
---
+++ db/migrate/20130711144238_create_teachers.rb
@@ -0,0 +1,17 @@
+require_relative '../config'
+
+class CreateTeachers < ActiveRecord::Migration
+  def change
+    # HINT: checkout ActiveRecord::Migration.create_table
+    create_table :teachers do |t|
+      t.string :first_name
+      t.string :last_name
+      t.string :email
+      t.string :phone
+    end
+    
+    add_column :students do |t|
+      t.integer :teacher_id
+    end
+  end
+end
